eea28a3b6d0aadf6b261f743e18c9e153919a8ec
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-19 19:41:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def done(client,message):
  path = f"{message.from_user.id}" + ".pdf"
  images[0].save(path, save_all = True, append_images = images[1:])
  
- await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here is your PDF !!\n*PDF Created by:- @epusthakalaya_bots*")
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here is your PDF !!\n**PDF Created by:- @epusthakalaya_bots**")
  os.remove(path)
  
  
